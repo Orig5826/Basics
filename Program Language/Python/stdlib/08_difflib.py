306387913e5c4c530ddd33e39ec45265ff0b4ca2
jianfeng <Orig5826@163.com>
2018-05-15 10:40:13 +0800

    将difflib的脚本，尝试修改为命令行可以接受参数进行比较的

--- Program Language/Python/stdlib/08_difflib.py
+++ Program Language/Python/stdlib/08_difflib.py
@@ -16,27 +16,37 @@ import sys
 # I am Bruce
 # 2018/5/14
 # '''.splitlines(keepends=True)
-# diffresult = difflib.unified_diff(str1,str2) 
+# diffresult = difflib.unified_diff(str1,str2)
 # sys.stdout.writelines(diffresult)
 
-# 用于比较的两个文件
-file = '01_urllib.py'
-file_patch = '01_urllib_2.py'
 
-# 打开需要比较的文件
-with open(file,encoding='utf-8') as f:
-    read_file = f.read()
-f.close()
-with open(file_patch,encoding='utf-8') as f:
-    read_file_patch = f.read()
-f.close()
+def diff(file, file_patch):
+    # 用于比较的两个文件
+    # file = '01_urllib.py'
+    # file_patch = '01_urllib_2.py'
 
-# ---------------------------------------
-# unified_diff参数解析
-# @param n 表示在生成diff文件中，内容不同行的
-# 			显示其上下相同内容多少行
-#			n = 0 表示不显示相同行
-diffresult = difflib.unified_diff(read_file.splitlines(keepends=True),read_file_patch.splitlines(keepends=True),
-fromfile=file,tofile=file_patch,n = 0)
+    # 打开需要比较的文件
+    with open(file, encoding='utf-8') as f:
+        read_file = f.read()
+    f.close()
+    with open(file_patch, encoding='utf-8') as f:
+        read_file_patch = f.read()
+    f.close()
 
-sys.stdout.writelines(diffresult)
+    # ---------------------------------------
+    # unified_diff参数解析
+    # @param n 表示在生成diff文件中，内容不同行的
+    # 			显示其上下相同内容多少行
+    #			n = 0 表示不显示相同行
+    diffresult = difflib.unified_diff(read_file.splitlines(keepends=True), read_file_patch.splitlines(keepends=True),
+                                      fromfile=file, tofile=file_patch, n=0)
+
+    sys.stdout.writelines(diffresult)
+
+
+if __name__ == "__main__":
+    if(len(sys.argv) == 3):
+        # 显示当前两个文件的比较结果
+        diff(sys.argv[1], sys.argv[2])
+    else:
+        print("Usage: 08_difflib.py fromfile tofile")
