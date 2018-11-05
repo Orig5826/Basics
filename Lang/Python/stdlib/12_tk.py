49d0f66e6e07fb0670fd8e2e92d8db1851eef9da
jf_HS <Orig5826@163.com>
2018-11-05 16:36:45 +0800

    上传tk练习

---
+++ Lang/Python/stdlib/12_tk.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+wind = tk.Tk()
+wind.title("tk 小程序")
+wind.geometry('300x200+500+300')
+
+# list
+ll = ["C", "C++", "Java", "Python", "Lua", "R"]
+lbox1 = tk.Listbox(wind)
+for item in ll:
+    lbox1.insert(0, item)
+
+
+
+lbox1.pack()
+wind.mainloop()
