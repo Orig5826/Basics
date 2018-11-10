dd1dbe6dac2990da0ceefbc1c47195ccc12506f1
jf_t96e <Orig5826@163.com>
2018-11-10 14:02:10 +0800

    上传python代码，saleae logic i2c辅助工具。带有GUI的(tkinter版本)

--- Lang/Python/stdlib/013_tkinter_2.py
+++ Lang/Python/stdlib/013_tkinter_2.py
@@ -1,12 +1,8 @@
 
-from tkinter import *
-from tkinter.filedialog import askopenfilename
+import tkinter as tk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 import csv
-
-# ----- 全局变量 ------
-# 文件名称
-global cmd
-# 命令的核心操作
+import time
 
 
 def Saleae_Logic_I2C_csv_parse(csv_fname):
@@ -14,6 +10,7 @@ def Saleae_Logic_I2C_csv_parse(csv_fname):
         逻辑分析仪，I2C导出的数据csv格式。
         可以通过该脚本直接查看收发内容
     '''
+    ss = ''
     with open(csv_fname, newline='') as csvfile:
         reader = csv.reader(csvfile)
         Packet_ID = -1
@@ -22,37 +19,69 @@ def Saleae_Logic_I2C_csv_parse(csv_fname):
                 continue
 
             if row[1] != str(Packet_ID):
-                print('')
+                # print('')
+                ss += "\n"
                 Packet_ID += 1
                 if row[4] == 'Write':
-                    print('-> ', end='')
+                    # print('-> ', end='')
+                    ss += "-> "
                 elif row[4] == 'Read':
-                    print('<- ', end='')
+                    # print('<- ', end='')
+                    ss += "<- "
                 else:
                     pass
             data = row[3]
-            print(data[2:], end=' ')
+            # print(data[2:], end=' ')
+            ss += data[2:] + ' '
+    return ss
 
 
 def open_file():
     file_path = askopenfilename(filetypes=[("文本文件", ".csv"), ("全部", ".*")])
     path.set(file_path)
+    cmd.set('请点击"开始转换"')
 
 
-def translate():
+def translate(save_mode=0):
+    # 将输出文件放在和源文件同一个目录下
     file_name = path.get()
-    Saleae_Logic_I2C_csv_parse(file_name)
+    # 开始转换
+    ss = Saleae_Logic_I2C_csv_parse(file_name)
+    if save_mode == 0:
+        file_output = file_name.rsplit('.', maxsplit=1)
+        # 获取当前时间
+        now = time.strftime("_%Y%m%d_%H%M%S")
+        file_output = file_output[0] + now + '.txt'
+    elif save_mode == 1:
+        # 通过对话框打开文件
+        file_output = asksaveasfilename(
+            defaultextension='.txt', filetypes=[("文本文件", ".txt")],
+            initialfile='saleae_logic_i2c.txt')
+    else:
+        return False
+    with open(file_output, 'w') as ff:
+        ff.write(ss)
+    ff.close()
+    cmd.set('转换完成')
 
 
-root = Tk()
-path = StringVar()
-cmd = StringVar()
+# tk实例化
+root = tk.Tk()
+path = tk.StringVar()
+cmd = tk.StringVar()
 
-Entry(root, textvariable=path).grid(row=0, column=0, columnspan=3, ipadx=30)
-Button(root, text="选择文件", command=open_file).grid(row=0, column=4)
+# 添加控件
+root.title('Saleae辅助工具 v1.0')
+root.iconbitmap("./res/apaki.ico")
+root.resizable(False, False)  # 大小不可变
+cmd.set('请选择文件...')
 
-Label(root, text='').grid(row=1, column=0, columnspan=3, ipadx=30)
-Button(root, text="开始转换", command=translate).grid(row=1, column=4)
+# 第一行
+tk.Entry(root, textvariable=path).grid(row=0, column=0, columnspan=2, ipadx=50)
+tk.Button(root, text="选择文件", command=open_file).grid(row=0, column=2)
+# 第二行
+tk.Label(root, textvariable=cmd).grid(row=1, column=0, columnspan=2, ipadx=50)
+tk.Button(root, text="开始转换", command=lambda: translate(1)).grid(row=1, column=2)
 
-
+# tk运行
 root.mainloop()
---
+++ Lang/Python/stdlib/016_time.py
@@ -0,0 +1,4 @@
+import time
+
+now = time.strftime("_%Y-%m-%d_%H:%M:%S")
+print(now)
