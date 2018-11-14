86af4c6f16288e3966d4f6637d6e5b41830e677b
jf_HS <Orig5826@163.com>
2018-11-14 10:58:42 +0800

    添加py_json示例，修改item002_translate使得其能够正常翻译单词。

---
+++ Lang/Python/stdlib/018_json.py
@@ -0,0 +1,24 @@
+
+import json
+"""
+    在shell中可以命令行直接将某json文件内容格式化显示
+    cat *.json | python -m json.tool
+"""
+ret = json.dumps({'Name':'Bob','Grades':{'Math':100,'Physics':100}},indent=4)
+print(ret)
+
+# ----------------------------------
+# 文件解析
+fname = './res/trans.json'
+print('-> 解析文件内容:',fname)
+
+with open(fname,encoding='utf-8') as ff:
+    fp_json = ff.read()
+ff.close()
+
+print('-------------------------')
+js = json.loads(fp_json)
+# print(js)
+result = '\n'.join(js['basic']['explains'])
+print(result)
+print('-------------------------')
--- Lang/Python/stdlib/item002_translate.py
+++ Lang/Python/stdlib/item002_translate.py
@@ -8,7 +8,7 @@ import httplib
 
 # coding=utf8
 appKey = '0b13f5d30d83bf9d'
-secretKey = 'B5ztskhIh6vqPLW3mQSwxdv8U9kQYdsX
+secretKey = 'B5ztskhIh6vqPLW3mQSwxdv8U9kQYdsX'
 
 
 httpClient = None
@@ -41,60 +41,79 @@ finally:
 """
 
 # 2018.11.13 by jianfeng
-# 修改未python3 示例
-# python2 -> python3
-# md5 -> hashlib.md5
-# httplib -> http.client
-#  rllib.quote ->  urllib.parse.quote
+# 1. 直接修改为python3 示例(参考网上资料)
+#   python2 -> python3
+#   md5 -> hashlib.md5
+#   httplib -> http.client
+#   rllib.quote ->  urllib.parse.quote
+# 
+# 2. 利用python自带的2to3工具。
+# 将2to3复制到需要转换的脚本目录下
+# 使用命令 2to3 -w *.py
+# 则会直接生成同名的python3脚本，py2被写入备份*.bak
+# (当然了这个工具能够实现大部分转换，但偶尔也会出问题
+# 那时候便需要手工调试了)
 import random
 import urllib
 import hashlib
 import http.client
 import json
 
-# 注意点
-# 1.编码都是utf8的
-# 2. appKey和secretKey都是从有道智云申请的你自己的
-# 有道智云https://ai.youdao.com/
-appKey = '*b13f5d30d83bf9d'
-secretKey = 'EUCRCf988ThQt8Fv2WaFv2W3wOaVTmj*'
+
+def translate(context):
+    # 注意点
+    # 1.编码都是utf8的
+    # 2. appKey和secretKey都是从有道智云申请的你自己的
+    # 有道智云https://ai.youdao.com/
+    appKey = '0b13f5d30d83bf9d'
+    secretKey = 'EUCRCf988ThQt8Fv2WaFv2W3wOaVTmj9'
+
+    httpClient = None
+    myurl = '/api'
+    q = context
+    fromLang = 'EN'
+    toLang = 'zh-CHS'
+    salt = random.randint(1, 65536)
+    # 组织签名
+    sign = appKey + q + str(salt) + secretKey
+    # 计算md5
+    sign = hashlib.md5(sign.encode()).hexdigest()
+
+    myurl = myurl+'?appKey='+appKey+'&q=' + \
+        urllib.parse.quote(q)+'&from='+fromLang+'&to='+toLang + \
+        '&salt='+str(salt)+'&sign='+sign
+
+    try:
+        httpClient = http.client.HTTPConnection('openapi.youdao.com')
+        httpClient.request('GET', myurl)
+
+        # response是HTTPResponse对象
+        response = httpClient.getresponse()
+        jsonResponse = response.read().decode('utf-8')
+        # print(jsonResponse)
+
+        # 将json格式的结果转换字典结构
+        js = json.loads(jsonResponse)
+        # print(js)
+        
+        # 取得翻译后的文本结果
+        result = '\n'.join(js['basic']["explains"])
+        # print(dst)
+        return result
+        
+    except Exception as e:
+        print(e)
+    finally:
+        if httpClient:
+            httpClient.close()
 
 
-httpClient = None
-myurl = '/api'
-q = 'good'
-fromLang = 'EN'
-toLang = 'zh-CHS'
-salt = random.randint(1, 65536)
-# 组织签名
-sign = appKey + q + str(salt) + secretKey
-# 计算md5
-sign = hashlib.md5(sign.encode()).hexdigest()
-
-myurl = myurl+'?appKey='+appKey+'&q=' + \
-    urllib.parse.quote(q)+'&from='+fromLang+'&to='+toLang + \
-    '&salt='+str(salt)+'&sign='+sign
-
-try:
-    httpClient = http.client.HTTPConnection('openapi.youdao.com')
-    httpClient.request('GET', myurl)
-
-    # response是HTTPResponse对象
-    response = httpClient.getresponse()
-    jsonResponse = response.read().decode('utf-8')
-    print(jsonResponse)
-
-    js = json.loads(jsonResponse)  # 将json格式的结果转换字典结构
-    dst = str(js["explain"])  # 取得翻译后的文本结果
-    print(dst)
-
-except Exception as e:
-    print(e)
-finally:
-    if httpClient:
-        httpClient.close()
-
-"""
-这个json报文的解析还有问题，先学习一下报文解析之后，再开始操作吧
-{"tSpeakUrl":"http://openapi.youdao.com/ttsapi?q=%E5%A5%BD&langType=zh-CHS&sign=9DEB8FE960F816763F977691C429078B&salt=1542121873261&voice=4&format=mp3&appKey=0b13f5d30d83bf9d","web":[{"value":["好的","善","良好","商品"],"key":"good"},{"value":["耶稣受难节","耶稣受难日","受难节","受难日"],"key":"Good Friday"},{"value":["吉芬商品","吉芬物品","吉芬品","季芬财"],"key":"Giffen Good"}],"query":"good","translation":["好"],"errorCode":"0","dict":{"url":"yddict://m.youdao.com/dict?le=eng&q=good"},"webdict":{"url":"http://m.youdao.com/dict?le=eng&q=good"},"basic":{"us-phonetic":"ɡʊd","phonetic":"gʊd","uk-phonetic":"gʊd","wfs":[{"wf":{"name":"比较级","value":"better"}},{"wf":{"name":"最高级","value":"best"}}],"uk-speech":"http://openapi.youdao.com/ttsapi?q=good&langType=en&sign=06AC4C6ACF1C7E672CB5038DD1C6E0F6&salt=1542121873261&voice=5&format=mp3&appKey=0b13f5d30d83bf9d","explains":["adj. 好的；优良的；愉快的；虔诚的","n. 好处；善行；慷慨的行为","adv. 好","n. (Good)人名；(英)古德；(瑞典)戈德"],"us-speech":"http://openapi.youdao.com/ttsapi?q=good&langType=en&sign=9828888B8EF683D1DF7BFA83AF07340A&salt=1542121873262&voice=6&format=mp3&appKey=0b13f5d30d83bf9d"},"l":"EN2zh-CHS","speakUrl":"http://openapi.youdao.com/ttsapi?q=good&langType=en&sign=06AC4C6ACF1C7E672CB5038DD1C6E0F6&salt=1542121873261&voice=4&format=mp3&appKey=0b13f5d30d83bf9d"}
-"""
+if __name__ == '__main__':
+    while True:
+        context = input('清输入英文单词(输入q退出):')
+        # context = 'good'
+        if context == 'q':
+            break;
+        ret = translate(context)
+        print(ret)
+    print('退出翻译')
