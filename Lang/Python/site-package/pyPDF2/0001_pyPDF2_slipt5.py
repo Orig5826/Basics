0faa80491d8d2350315ed2b684294548f1942c85
jf_HS <Orig5826@163.com>
2018-12-17 18:15:03 +0800

    修改为命令行方式

--- Lang/Python/site-package/pyPDF2/0001_pyPDF2_slipt5.py
+++ Lang/Python/site-package/pyPDF2/0001_pyPDF2_slipt5.py
@@ -1,26 +1,33 @@
 # -*- code=utf_8 -*-
 import PyPDF2 as pdf
 import os
+import sys
 
-inputfile = r".\安全模块设计文档20180612_1536_V1.0.pdf"
-outputpath = ".\\split\\"
+def split_pdf(inputfile):
+	outputpath = ".\\split\\"
+	
+	reader = pdf.PdfFileReader(inputfile)
+	PageIndexMax = reader.getNumPages()
+	print("pages = %d" % PageIndexMax)
 
-reader = pdf.PdfFileReader(inputfile)
-PageIndexMax = reader.getNumPages()
-print("pages = %d" % PageIndexMax)
+	if not os.path.exists(outputpath):
+		os.mkdir(outputpath)
 
-if not os.path.exists(outputpath):
-    os.mkdir(outputpath)
+	# 每5页分割为一个单独的pdf文件
 
-# 每5页分割为一个单独的pdf文件
+	for i in range(0, PageIndexMax):
+		# 写入分割后的文件
+		if i % 5 == 0:
+			writer = pdf.PdfFileWriter()
+			split_pdf = outputpath + 'split_' + '{0:02d}'.format(i//5) + '.pdf'
+		page = reader.getPage(i)
+		writer.addPage(page)
+		if (i + 1) % 5 == 0:
+			with open(split_pdf, 'wb+') as fh:
+				writer.write(fh)
 
-for i in range(0, PageIndexMax):
-    # 写入分割后的文件
-    if i % 5 == 0:
-        writer = pdf.PdfFileWriter()
-        split_pdf = outputpath + 'split_' + '{0:02d}'.format(i//5) + '.pdf'
-    page = reader.getPage(i)
-    writer.addPage(page)
-    if (i + 1) % 5 == 0:
-        with open(split_pdf, 'wb+') as fh:
-            writer.write(fh)
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print('命令行参数错误')
+	inputfile = sys.argv[1]
+	split_pdf(inputfile)
