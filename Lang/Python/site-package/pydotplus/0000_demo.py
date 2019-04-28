41d9db5f6becb48798d9163991109e1ca632b012
jf_t96e <Orig5826@163.com>
2019-04-28 21:57:35 +0800

    pydotplus常用的命令基本学会，后续可以添加到我实际工程中了哈哈

--- Lang/Python/site-package/pydotplus/0000_demo.py
+++ Lang/Python/site-package/pydotplus/0000_demo.py
@@ -26,49 +26,67 @@ from pydotplus import graph_from_dot_data
 from pydotplus import Dot, Node, Edge
 
 
-graph = Dot(rankdir='LR')
+graph = Dot(rankdir='TB')
 graph.set_type('digraph')
 graph.set_name('Dot_Demo')
 
-data = {'start':'开始',
-        '1':'第一步',
-        '2':'第二步',
-        '3':'第三步',
-        'end':'结束'}
+data = {'start': '开始',
+        '1': '第一步',
+        '2': '第二步',
+        '3': '第三步',
+        'end': '结束'}
 
-dot_list = []
+node_name = tuple(data.keys())
 
+graph.set_node_defaults(shape='record')
 # 将结点和边关联
 i = 0
 for key, value in data.items():
-    if i == 2:
-        node = Node(key, label="{<b1> " + value + "|<b2>}", shape='record')
+    if i == 0 or i == len(data)-1:
+        # node label中{}的作用，相当于开辟新的作用域，且垂直水平方向会相对于当前来进行切换
+        """
+        digraph G {
+            node [shape=record];
+            1 [label="<left>左|{<top>上|<buttom>下}|<right>右"];
+        }
+        """
+        node = Node(key, shape='ellipse')
     else:
-        node = Node(key, label="{<b1> " + value + "|<b2>}", shape='record')
-    print(node.to_string())
-    dot_list.append(node)
-
-    if i != 0:
-        edge = Edge(old_node, node)
-        print(edge.to_string())
-        graph.add_edge(edge)
+        label_value = "\"<data> " + value + "|<next>\""
+        node = Node(key, label=label_value)
     
+    # print(node.get_name())
+    # node.set_shape('record')  # 直接配置默认了set_node_defaults
     graph.add_node(node)
-    old_node = node
+
+    if i == 0:
+        i += 1
+        continue
+    elif i == 1:
+        edge = Edge(node_name[i-1], node.get_name()+':data')
+    elif i == len(data) - 1:
+        edge = Edge(node_name[i-1]+':next', node.get_name())
+    else:
+        edge = Edge(node_name[i-1]+':next', node.get_name()+':data')
+    graph.add_edge(edge)
+    print(edge.to_string())
     i += 1
+
 
 # 2019.04.28 目前唯一的问题是不知道如何实现Edge
 # start:b2 -> 1:b1;
-edge = Edge(dot_list[3], dot_list[1])
-print(edge.to_string())
-graph.add_edge(edge)
+# 2019.04.28(20:52)哈哈，已经解决了
+# 原来Edge只需要传入node_name即可，没必要传入Node类
+# edge = Edge(node_name[3]+':next', node_name[1]+':data')
+# graph.add_edge(edge)
+# print(edge.to_string())
 
 
 ret = graph.write_raw("demo.dot")
-if ret != True:
+if ret is not True:
     print('生成demo.dot失败')
 ret = graph.write_svg("graph.svg")
-if ret != True:
+if ret is not True:
     print('生成graph.svg失败')
 
 os.system("graph.svg")
