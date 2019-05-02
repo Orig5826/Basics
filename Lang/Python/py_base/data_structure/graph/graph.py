d3b3d622efb60d47833f2ecfc7b9c1d501a289fc
jf_t96e <Orig5826@163.com>
2019-05-02 23:19:02 +0800

    py实现图的路径查找

---
+++ Lang/Python/py_base/data_structure/graph/graph.py
@@ -0,0 +1,123 @@
+
+from pydotplus import Dot, Node, Edge
+import os
+
+# 该图配置
+graph = {'A': ['B', 'C', 'F'],
+         'B': ['C', 'D'],
+         'C': ['D'],
+         'D': ['C'],
+         'E': ['F', 'D'],
+         'F': ['C']
+         }
+
+
+def dotgraph(graph):
+    __graph = Dot(rankdir='TB', fontname="Fangsong",
+                  fontcolor='blue', label="有向图的示例")
+    __graph.set_type('digraph')
+    __graph.set_name('digraph_demo')
+
+    __graph.set_node_defaults(
+        fontname="Fangsong", style='filled', fillcolor='yellow')
+    __graph.set_edge_defaults(fontname="Fangsong", color='black')
+
+    for key, value in graph.items():
+        # 若节点没有特殊label或者其他属性需求
+        # 可以直接以节点名称显示
+        # 直接标记方向，不用手动添加
+        # node = Node(key)
+        # __graph.add_node(node)
+        for v in value:
+            edge = Edge(key, v)
+            __graph.add_edge(edge)
+
+    ret = __graph.write_raw("demo.dot")
+    if ret is not True:
+        print('生成demo.dot失败')
+    ret = __graph.write_svg("demo.svg")
+    if ret is not True:
+        print('生成graph.svg失败')
+
+    # ret = __graph.write_png("demo.png")
+    # if ret is not True:
+    #     print('生成graph.png失败')
+
+    return __graph
+
+
+def find_path(graph, start, end, path=[]):
+    """
+        在图graph中找路径：
+        从顶点start到顶点end
+        走过的路径为path
+    """
+    path = path + [start]
+    # 3.0 若当找到路径尾部，则返回该路径
+    if start == end:
+        return path
+    # 1.0 判断当前顶点是否在图内
+    if start not in graph.keys():
+        return None
+    for node in graph[start]:
+        if node not in path:
+            # 2.0 以当前顶点为起点，继续找路径
+            newpath = find_path(graph, node, end, path)
+            # 4.0 返回该路径
+            if newpath:
+                return newpath
+    # 这个没有什么用吗 ？
+    # return path
+
+
+def find_all_paths(graph, start, end, path=[], paths=[]):
+    path = path + [start]
+    if start == end:
+        paths.append(path)
+        return path
+    if start not in graph.keys():
+        return None
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+    if paths == []:
+        return None
+    return paths
+
+
+def find_short_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if start not in graph.keys():
+        return None
+
+    shortpath = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath:
+                if not shortpath or len(newpath) < len(shortpath):
+                    shortpath = newpath
+    return shortpath
+
+
+if __name__ == '__main__':
+    result = find_path(graph, 'A', 'D')
+    print("1. 路径查找结果：", result)
+    print('---------------------------------')
+
+    result = find_all_paths(graph, 'A', 'D')
+    print("2. 全路径查找结果：", result)
+    print("路径个数：", len(result))
+    i = 1
+    for path in result:
+        print('路径{0:2d}为：{1}'.format(i, path))
+        i += 1
+    print('---------------------------------')
+
+    result = find_short_path(graph, 'A', 'D')
+    print("3. 查找最短路径：", result)
+
+    # 生成图表
+    dotgraph(graph)
