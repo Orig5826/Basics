d4434713a63f9ac8c21a6232fdcfa3a120448c67
jf_HS <Orig5826@163.com>
2019-06-21 18:55:02 +0800

    针对vs2017的del.bat脚本做了修改。另外.gitignore针对vs2017做了屏蔽

--- Project/USB/Windows/usbdisk/vs2017_del.bat
+++ Project/USB/Windows/usbdisk/vs2017_del.bat
@@ -1,25 +1,21 @@
 @echo off
 
-REM 1.0 获取当前工程所在的目录的名称
-for %%i in ("%cd%") do set name=%%~ni
-echo %name%
-
 REM 删除解决方案主目录下某些文件/夹
 REM vs2017 *.db等文件均放在.vs目录
 RD /Q /S  .\.vs
-RD /Q /S  .\Debug  .\Release  .\x64  .\x86  .\ARM
-REM 删除相应工程下的Debug及其目录下的编译产生的所有文件
-cd  .\%name%
-	RD /Q /S  .\Debug  .\Release  .\x64  .\x86  .\ARM
+RD /Q /S  .\Debug  .\Release .\x64  .\x86  .\ARM
+
+REM 方式1 获取当前工程所在的目录的名称
+REM for %%i in ("%cd%") do set name=%%~ni
+REM echo %name%
+
+REM 方式2 直接找出所有文件夹下的中间文件
+REM 随后删除
+for /f %%i in ('dir /B /ON /AD') do (
+echo %%i
+
+cd  .\%%i
+	RD /Q /S  .\Debug .\Release .\x64  .\x86  .\ARM
 	del /Q /S *.user
 	cd ..
-
-REM 其他工程目录也需要删除
-set name=%name%"_demo"
-cd  .\%name%
-	RD /Q /S  .\Debug  .\Release  .\x64  .\x86  .\ARM
-	del /Q /S *.user
-	cd ..
-
-REM 删除完毕
-REM pause
+)
