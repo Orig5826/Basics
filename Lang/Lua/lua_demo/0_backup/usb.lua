c60bc035973ae87d68ca5dabc524fc48ae511817
jf_HS <wenjf@hongis-ic.com>
2020-11-27 15:31:09 +0800

    修改lua -> lusbidsk 测试脚本

--- Lang/Lua/lua_demo/0_backup/usb.lua
+++ Lang/Lua/lua_demo/0_backup/usb.lua
@@ -1,3 +1,10 @@
+--[[
+	相关接口：
+	handle = usbdisk.open(symbolic_link)
+	bool = usbdisk.close(handle)
+	wrote_cnt = usbdisk.write(handle,table)
+	table = usbdisk.read(handle,size)
+]]--
 local usb = require("lusbdisk")
 
 function tbprint(tb,ss)
@@ -8,17 +15,28 @@ function tbprint(tb,ss)
 	
 	for i=1,#tb do
 		s = s .. string.format("%02x ",tb[i])
-		-- print(s)
 	end	
 	print(s)
 end
 
-dev = usb.open("HSEC    usb2uart_c1test 0002")
-cmd = {0x00,0x84,0x00,0x00,0x10}
-usb.write(dev,cmd)
-tbprint(cmd,"-> ")
 
-res = usb.read(dev,18)
-tbprint(res,"<- ")
+function usb_run(symbolic_link)
+	hdev = usb.open(symbolic_link)
+	if hdev == 0 then
+		print("open [" .. symbolic_link .."] device failed!")
+		os.exit(-1)
+	end
+	cmd = {0x00,0x84,0x00,0x00,0x10}
+	
+	cnt = 1
+	for i=1,cnt do
+		usb.write(hdev,cmd)
+		tbprint(cmd,"-> ")
+		res = usb.read(hdev,18)
+		tbprint(res,"<- ")
+	end
+	
+	usb.close(hdev)
+end
 
-usb.close(dev)
+usb_run("HSEC    usb2uart_c1test 0002")
