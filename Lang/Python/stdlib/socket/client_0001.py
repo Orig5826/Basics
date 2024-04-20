daf52469f334f4c277c63395f49d1b7c2495cf1f
jf-home <Orig5826@163.com>
2024-04-20 20:48:16 +0800

    中间有丢包，我不太理解是什么原因。

--- Lang/Python/stdlib/socket/client_0001.py
+++ Lang/Python/stdlib/socket/client_0001.py
@@ -4,14 +4,16 @@ import sys
 import random
 import time
 
+def time_items():
+    print("|  time(s)  |    KB   |   KB/s   |   MB/s   |   Mbps   |")
 
 def time_calc(time_diff, datalen):
-    print("{:%0.2d}s".format(time_diff),end="")
+    print("{:10.6f}".format(time_diff),end="")
     if time_diff != 0:
-        print("{:%0.2f}B/s".format(datalen/time_diff),end="")
-        print("{:%0.2f}KB/s".format(datalen/1024/time_diff),end="")
-        print("{:%0.2f}MB/s".format(datalen/1024/1024/time_diff),end="")
-        print("{:%0.2f}Mb/s".format(8*datalen/1024/1024/time_diff),end="")
+        print("{:10.3f}".format(datalen/1024),end="")
+        print("{:10.2f}".format(datalen/1024/time_diff),end="")
+        print("{:10.2f}".format(datalen/1024/1024/time_diff),end="")
+        print("{:10.2f}".format(8*datalen/1024/1024/time_diff),end="")
     print("")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,21 +29,27 @@ except:
 
 
 print('echo test by 1024 bytes!')
-print("")
-#while True:
-for i in range(10):
-    cmd = bytearray(1024)
-    for i in range(1024):
+time_items()
+
+# 测试包长
+package_size = 2048
+
+# 测试轮次
+count = 1000
+
+for i in range(count):
+    cmd = bytearray(package_size)
+    for i in range(package_size):
         cmd[i] = random.randint(0,255)
         
     start_time = time.time()
     s.send(cmd)
-    res = s.recv(1024)
+    res = s.recv(package_size)
     end_time = time.time()
     
     # 计算代码执行的时间
     time_diff = end_time - start_time
-    time_calc(time_diff, 1024)
+    time_calc(time_diff, package_size)
     
     # print(res.hex(' '))
 
